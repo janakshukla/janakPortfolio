effe800306f347c78d03f500e9b6c2a420488437
janakshukla <148837744+janakshukla@users.noreply.github.com>
2025-02-14 21:01:15 +0530

    Refactor Intro and Contact components: consolidate social media links and improve layout

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,31 +1,8 @@
-import { Github,Linkedin,FileUser,Instagram,Twitter } from 'lucide-react' 
-import resume from '../assets/janakshukla_resume.pdf'
 
 export const Contact = () => {
   return (
     <div id="contact"  className="backdrop-blur-sm bg-gray-900 text-2xl py-5 flex justify-center items-center select-none ">
 
-      <div className=' md:ml-48 flex gap-8' >
-      <Twitter  className='hover:text-blue-600 font-extrabold' 
-     
-     onClick={() => window.open('https://x.com/janakshukla2004')}
-     size={45} />
-
-      <Github className='hover:text-blue-600 ' 
-     
-      onClick={() => window.open('https://github.com/janakshukla')}
-      size={45} />
-      <Linkedin className='hover:text-blue-600 '
-       onClick={() => window.open('https://www.linkedin.com/in/janak-shukla-b25808257/')}
-        size={45} />
-      <Instagram className='hover:text-blue-600 ' 
-       onClick={() => window.open('https://www.instagram.com/janak.shukla.735/')}
-        size={45} />
-      <FileUser className='hover:text-blue-600 '
-        
-        onClick={() => window.open(resume)}
-        size={45} />
-      </div>
     </div>
 
   )
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,46 +2,85 @@ import { Link } from "react-router-dom";
 import image from "../assets/janak2.jpg";
 import { motion } from "motion/react";
 import { Presentation } from "lucide-react";
-import {Contact} from "./Contact";
+
+import { Github, Linkedin, FileUser, Instagram, Twitter } from "lucide-react";
+import resume from "../assets/janakshukla_resume.pdf";
+
 import { Footprints } from "lucide-react";
 function Intro() {
   return (
     <>
-    <motion.div className="h-full  md:flex  justify-between ">
-      <div className="overflow-hidden h-screen md:w-1/2  ">
-        <motion.img
-          whileHover={{ scale: 1.1 }}
-          src={image}
-          alt="janak"
-          className="object-cover filter grayscale hover:brightness-0 hover:filter-none duration-500 "
-        />
-      </div>
-      <div className=" flex justify-center items-center text-white md:w-1/2 ">
-        <div >
+      <motion.div className="h-full  md:flex  justify-between ">
+        <div className="overflow-hidden md:h-screen lg:w-1/2  ">
+          <motion.img
+            whileHover={{ scale: 1.1 }}
+            src={image}
+            alt="janak"
+            className="object-cover filter grayscale hover:brightness-0 hover:filter-none duration-500 "
+          />
+        </div>
+        <div className=" flex justify-center items-center text-white md:w-1/2 ">
           <div>
-            <h1 className="text-4xl font-serif text-center leading-loose  font-bold mt-4">
-              I am Janak shukla
-            </h1>
-            <p className="text-center sm:text-lg leading-tight mt-2">
-              I am a full stack developer with the knowledge in
-              Nextjs,MongoDB,Prisma,Expressjs,Reactjs,Nodejs and many more😊.
-            </p>
-          </div>
-          <div className="flex justify-center mt-8 items-center gap-24 ">
-            <Link to="/about" className="hover:text-blue-800">
-              <Footprints className="md:size-14 size-9 " />
-              About
-            </Link>
-            <Link to="/projects" className="hover:text-blue-800">
-              <Presentation className="md:size-14 size-9 " />
-              Projects
-            </Link>
+            <div>
+              <h1 className="text-4xl font-serif text-center leading-loose  font-bold md:mt-4">
+                I am Janak shukla
+              </h1>
+              <p className="text-center sm:text-lg leading-tight mt-2">
+                I am a full stack developer with the knowledge in
+                Nextjs,MongoDB,Prisma,Expressjs,Reactjs,Nodejs and many more😊.
+              </p>
+            </div>
+            {/* navigation */}
+            <div className="flex justify-center mt-8 items-center gap-24 ">
+              <Link to="/about" className="hover:text-blue-800">
+                <Footprints className="md:size-14 size-9 " />
+                About
+              </Link>
+              <Link to="/projects" className="hover:text-blue-800">
+                <Presentation className="md:size-14 size-9 " />
+                Projects
+              </Link>
+            </div>
+            {/* contact */}
+            <div className="mt-8" >
+              <div className=" items-center justify-center flex gap-8">
+                <Twitter
+                  className="hover:text-blue-600 font-extrabold"
+                  onClick={() => window.open("https://x.com/janakshukla2004")}
+                  size={45}
+                />
+
+                <Github
+                  className="hover:text-blue-600 "
+                  onClick={() => window.open("https://github.com/janakshukla")}
+                  size={45}
+                />
+                <Linkedin
+                  className="hover:text-blue-600 "
+                  onClick={() =>
+                    window.open(
+                      "https://www.linkedin.com/in/janak-shukla-b25808257/"
+                    )
+                  }
+                  size={45}
+                />
+                <Instagram
+                  className="hover:text-blue-600 "
+                  onClick={() =>
+                    window.open("https://www.instagram.com/janak.shukla.735/")
+                  }
+                  size={45}
+                />
+                <FileUser
+                  className="hover:text-blue-600 "
+                  onClick={() => window.open(resume)}
+                  size={45}
+                />
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-   
-    </motion.div>
-    
+      </motion.div>
     </>
   );
 }
