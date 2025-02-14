75f63c9ef77c927cbfff6b6838766294e891bc07
janakshukla <148837744+janakshukla@users.noreply.github.com>
2025-02-14 21:17:44 +0530

    updaed resume

--- src/components/Contact.jsx
+++
@@ -1,9 +0,0 @@
-
-export const Contact = () => {
-  return (
-    <div id="contact"  className="backdrop-blur-sm bg-gray-900 text-2xl py-5 flex justify-center items-center select-none ">
-
-    </div>
-
-  )
-}
