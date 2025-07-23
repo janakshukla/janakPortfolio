fe06708e122f5449fa7c1f1c3dfdfaaf3f0fd3a5
janakshukla <148837744+janakshukla@users.noreply.github.com>
2025-07-23 16:19:36 +0530

    Enhance Hero component: add social media links and improve layout for better accessibility

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,27 @@
+import { Github, Linkedin, Twitter } from 'lucide-react';
 import img from '../assets/image.png';
 
 const Hero = () => {
   return (
     <div>
-       <div>
+       <div className='flex justify-between' >
        <img
         className='size-40 rounded-3xl object-cover shadow-2xl shadow-[#00000040] border-2 border-[#ffffff20] hover:scale-105 transition-all duration-500 ease-in-out' 
          src={img} 
          alt="this is hero image" />
-         {/*  */}
+         {/* contact section */}
+         <div className='flex justify-end gap-6 '>
+           <a  className='hover:bg-slate-600 p-2 rounded-2xl h-fit' href="https://github.com/janakshukla/">
+            <Github className='size-6   text-white ' />
+            </a>
+           <a className='hover:bg-slate-600 p-2 rounded-2xl h-fit' href="https://x.com/janakshukla2004/">
+            <Twitter className='size-6  text-white ' />
+            </a>
+           <a className='hover:bg-slate-600 p-2 rounded-2xl h-fit' href="https://www.linkedin.com/in/janak-shukla-b25808257/">
+            <Linkedin className='size-6  text-white ' />
+            </a>
+
+         </div>
        </div>
         <h1 
         className=''
