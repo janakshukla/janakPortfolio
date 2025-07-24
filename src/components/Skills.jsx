33abd9929649ae5ec38d825afd27fe46b0a74e96
janak shukla <148837744+janakshukla@users.noreply.github.com>
2025-07-24 18:30:41 +0530

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -21,7 +21,7 @@ const Skills = () => {
     "Postman",
   ];
   return (
-    <div className="lg:mt-12  ">
+    <div className="lg:mt-12 mt-4 ">
       <h1 className=" text-2xl font-medium mb-3 bg-gradient-to-r from-blue-500 to-cyan-600 text-transparent bg-clip-text">
         Skills
       </h1>
