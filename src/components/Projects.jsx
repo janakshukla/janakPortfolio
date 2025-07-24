2bb0e808946972708dc26c38a2976747dce794dc
janak shukla <148837744+janakshukla@users.noreply.github.com>
2025-07-24 18:31:16 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -32,7 +32,7 @@ export const Projects = () => {
     },
   ];
   return (
-    <div className="lg:mt-12  ">
+    <div className="lg:mt-12 mt-4 ">
       <h1 className=" text-2xl font-medium mb-3 bg-gradient-to-r from-blue-500 to-cyan-600 text-transparent bg-clip-text">
         Projects
       </h1>
